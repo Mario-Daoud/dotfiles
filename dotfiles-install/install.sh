8c2b795b55be18053d1a970a6ea4d11e9e9a6d33
Mario Daoud <mario.daoud.3002@gmail.com>
2024-01-28 16:24:01 +0100

    add install

---
+++ dotfiles-install/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone --bare https://github.com/Mario-Daoud/dotfiles.git $HOME/.dotfiles
+
+alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+
+dotfiles checkout
+
+dotfiles config --local status.showUntrackedFiles no
